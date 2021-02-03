3932a6a2c0063ebd8965368b568e5e055ca9712b
yuami <yuami99@gmail.com>
2021-02-03 11:29:11 +0100

    CD model and migration

--- cds/models.py
+++ cds/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Band(models.Model):
     name = models.CharField(max_length=50)
-    creation_date = models.DateTimeField(verbose_name='Creation date', default=timezone.now())
+    creation_date = models.DateTimeField(verbose_name='Creation date', auto_now_add=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+
+class CD(models.Model):
+    band_id = models.ForeignKey(Band, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField(verbose_name='Publication date')
+    total_songs = models.IntegerField(verbose_name='Total songs')
+
+    def __str__(self):
+        return self.title
