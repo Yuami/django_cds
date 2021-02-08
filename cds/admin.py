8309d5d48ccc535b7db993a10e45da541fb56a57
yuami <yuami99@gmail.com>
2021-02-08 10:18:30 +0100

    Autocomplete fk, inlines

--- cds/admin.py
+++ cds/admin.py
@@ -3,18 +3,28 @@ from django.contrib import admin
 from cds.models import Band, Cd, Song
 
 
+class CdInline(admin.TabularInline):
+    model = Cd
+
+
+class SongInline(admin.TabularInline):
+    model = Song
+
+
 @admin.register(Band)
 class BandAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
+    inlines = [CdInline]
 
 
 @admin.register(Cd)
-class Cddmin(admin.ModelAdmin):
-    pass
+class CdAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    autocomplete_fields = ['band']
+    inlines = [SongInline]
 
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
-    pass
-
-
+    search_fields = ['title']
+    autocomplete_fields = ['cd']
